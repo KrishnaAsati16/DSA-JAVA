debe38912fc1ba7c33df3ae6f0d8008511ee09cd
KrishnaAsati16 <krishnaasati2006@gmail.com>
2025-11-18 01:16:58 +0530

    Patterns

---
+++ Pattern_Printing/src/Patterns/pyramid.java
@@ -0,0 +1,22 @@
+package Patterns;
+
+import java.util.Scanner;
+
+public class pyramid {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int nsp = n-1, nst = 1;
+        for(int i=1;i<=n;i++){ // lines
+            for(int j=1;j<=nsp;j++){ // spaces
+                System.out.print(" "+" ");
+            }
+            for(int j=1;j<=nst;j++){ // stars
+                System.out.print("*"+" ");
+            }
+            nsp--;
+            nst += 2;
+            System.out.println();
+        }
+    }
+}
