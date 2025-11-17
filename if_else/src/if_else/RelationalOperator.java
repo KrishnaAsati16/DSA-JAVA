2662f38a16049f7b787e4a119da0aa8a0c8c3ec8
KrishnaAsati16 <krishnaasati2006@gmail.com>
2025-11-17 14:07:49 +0530

    If_Else

---
+++ if_else/src/if_else/RelationalOperator.java
@@ -0,0 +1,9 @@
+package if_else;
+
+public class RelationalOperator {
+    public static void main(String[] args) {
+    boolean x = (5>3);
+    System.out.println(x);
+
+}
+}
---
+++ if_else/src/if_else/StudentOfCodes.java
@@ -0,0 +1,17 @@
+package if_else;
+import java.util.Scanner;
+public class StudentOfCodes {
+    static void main(String[] args) {
+        Scanner sb = new Scanner(System.in);
+        System.out.print("Enter Number: ");
+        int a = sb.nextInt();
+        if(a%5 == 0 && a%3 == 0)
+            System.out.println("Apurva");
+        else if (a%3 == 0)
+            System.out.println("Banu");
+        else if(a%5 == 0)
+            System.out.println("Riya");
+        else
+            System.out.println("Isha");
+    }
+}
