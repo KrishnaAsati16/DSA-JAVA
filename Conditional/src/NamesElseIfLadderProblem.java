b1a35036bc309856db4c329b630896fb8ee632d8
KrishnaAsati16 <krishnaasati2006@gmail.com>
2025-12-16 13:45:40 +0530

    condition

---
+++ Conditional/src/NamesElseIfLadderProblem.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class NamesElseIfLadderProblem {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Number: ");
+        int n = sc.nextInt();
+        if(n%5 == 0 && n%3 == 0) System.out.println("Aditya");
+        else if(n%3 == 0) System.out.println("Arjun");
+        else if(n%5 == 0) System.out.println("Tania");
+        else System.out.println("Eva");
+
+//        if(n%5 == 0 && n%3 != 0) System.out.println("l");
+//        if(n%3 == 0 && n%5 != 0) System.out.println("A");
+//        if(n%5 == 0 && n%3 == 0) System.out.println("k");
+//        if(n%5 != 0 && n%3 != 0) System.out.println("E");
+    }
+}
