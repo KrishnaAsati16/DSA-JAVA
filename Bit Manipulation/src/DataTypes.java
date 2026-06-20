3404b8f115007885331b3d6fab3aa3811f0a04c8
Krishna Asati <krishnaasati2006@gmail.com>
2026-06-20 22:31:15 +0530

    Bit Manipulation
    Implement DataTypes class demonstrating byte and int manipulation.

---
+++ Bit Manipulation/src/DataTypes.java
@@ -0,0 +1,41 @@
+public class DataTypes {
+
+     // byte = 8 bits
+    // byte range = -128 to 127
+
+
+
+    static void main(String[] args) {
+        byte x = 127;
+        x+=1;
+        System.out.println(x);
+//        ans  = -128
+
+
+        int y = Integer.MAX_VALUE;
+        y+=10;
+        System.out.println(y);
+
+
+        // how -ve numbers are stored
+
+
+
+
+
+
+        // << and >> operator
+//        let x = 10110
+
+        // right shift
+
+        // x>>1 = 1011
+        // x>>3 = 10
+
+        // left shift
+
+        // x<<1 = 101100
+        // x<<2 = 1011000
+
+    }
+}
